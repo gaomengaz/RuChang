cef53f61305bdb11118cc5c315e4c93d3cee8cbe
gaomeng <gaomeng@jijiankang.cn>
2018-01-10 09:44:49 +0800

    Initial Commit

---
+++ RuChang/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RuChang
+//
+//  Created by gaomeng on 2018/1/10.
+//  Copyright © 2018年 gaomeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
